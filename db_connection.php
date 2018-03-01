cd4893ccbbb6784fd69fbcca5520e3818f8d095d
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2018-03-01 01:05:01 +0100

    some_changes

--- db_connection.php
+++ db_connection.php
@@ -6,31 +6,12 @@
 	
 
 	$servername = "localhost";
-    $username = "lesson_learned";
-    $password = "Ewq2RVHUxO6FN64Y";
-    $dbname = "lesson_learned";
+    $username = "root";
+    $password = "root";
+    $dbname = "db_name";
 
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password );	
+	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password );	
 ?>
-
-<?php 
-	/*
-	abstract class db_connection 
-	{
-		
-		function db_connection()
-		{
-			$servername = "localhost";
-   			$username = "lesson_learned";
-   			$password = "Ewq2RVHUxO6FN64Y";
-   			$dbname = "lesson_learned";
-   			$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password );	
-		}
-	}
-*/
- ?>
-
-
 
   
 	
--- get_ip.php
+++ get_ip.php
@@ -1,7 +1,7 @@
 <?php
-// Function to get the client IP address
+
 	date_default_timezone_set('Europe/Warsaw');
-	//data disiejsza
+
 	function get_client_ip() {
 	    $ipaddress = '';
 	    if (isset($_SERVER['HTTP_CLIENT_IP']))
@@ -23,27 +23,4 @@
 
 	$ip = get_client_ip();
 
-	
-
-//$today = date("m.d.y");
-//$now = date("H.i.s");
-//$ip = get_client_ip();
-//$structure = './session';
-
-
-// Desired folder structure
-// tworzenie folderu dla operacij 
-// to mkdir() must be specified.
-/* if (!mkdir($structure, 0777, true)) {
-//     die('Failed to create folders...');
- } */
-// Treść pliku
-
-//$txt = $ip."_".$now."_".$today;
-//$myfile = fopen("./session/$txt.txt", "w") or die("Plik nie istnieje !");
-	
-//	fwrite($myfile, $txt);
-//	fclose($myfile);
-
-	//echo "log file was created";
 ?>
